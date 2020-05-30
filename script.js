d5d66ba26c0318899b6e266f8e1b7835dcdbbe2e
Danny Demosi <ddemosi@remodal.io>
2020-05-30 09:38:56 -0600

    draft 3

--- script.js
+++ script.js
@@ -1,43 +1,35 @@
-let modal = document.querySelector(".modal");
-let editButton = document.querySelector('.profile__edit-button');
-let closeEditButton = document.querySelector('.form__exit');
+const modal = document.querySelector(".modal");
+const editButton = document.querySelector('.profile__edit-button');
+const closeEditButton = document.querySelector('.form__exit');
+const profileName = document.querySelector('.form__input-name');
+const profileRole = document.querySelector('.form__input-about');
+const profileSaveButton = document.querySelector('.form__save-button');
+const profileNameContent = document.querySelector('.profile__name').textContent;
+const profileRoleContent = document.querySelector('.profile__subtitle').textContent;
+const defaultName = document.querySelector('.profile__name');
+const defaultRole = document.querySelector('.profile__subtitle');
 
-editButton.addEventListener("click", function() {
-    modal.style.display = "block";
-} );
+function closeModal() { 
+    modal.classList.remove('modal_display_visible');
+    modal.classList.add('modal_display_none');
+}
 
-closeEditButton.addEventListener('click', function() {
-    modal.style.display = "none";
-});
+function openModal() {
+    modal.classList.remove('modal_display_none');
+    modal.classList.add('modal_display_visible');
+}
 
-window.addEventListener('click', function(e) {
-    if (e.target == modal) {
-        modal.style.display = "none";
-    }
-});
+editButton.addEventListener("click", openModal);
 
-let profileName = document.querySelector('#profile-name');
-let profileRole = document.querySelector('#profile-role');
-let profileSaveButton = document.querySelector('.form__save-button');
-let profileNameContent = document.querySelector('.profile__name').textContent;
-let profileRoleContent = document.querySelector('.profile__subtitle').textContent;
+closeEditButton.addEventListener('click', closeModal);
 
 profileName.setAttribute("value", profileNameContent);
 profileRole.setAttribute("value", profileRoleContent);
 
-// profileName.addEventListener("focus", function(e) {
-//     e.target.value = "";
-// });
-
-// profileRole.addEventListener('focus', function(e) {
-//     e.target.value = "";
-// })
-
 profileSaveButton.addEventListener("click", function() {
-    let name = document.querySelector('.profile__name');
-    let role = document.querySelector('.profile__subtitle');
-    name.innerHTML = profileName.value;
-    role.innerHTML = profileRole.value;
+    
+    defaultName.textContent = profileName.value;
+    defaultRole.textContent = profileRole.value;
 
-    modal.style.display = "none";
+    closeModal();
 })
