49f02b4cb9240ce24b4084750f8b40970ab8714d
Danny Demosi <ddemosi@remodal.io>
2020-05-31 08:19:18 -0600

    draft four

--- script.js
+++ script.js
@@ -4,32 +4,26 @@ const closeEditButton = document.querySelector('.form__exit');
 const profileName = document.querySelector('.form__input-name');
 const profileRole = document.querySelector('.form__input-about');
 const profileSaveButton = document.querySelector('.form__save-button');
-const profileNameContent = document.querySelector('.profile__name').textContent;
-const profileRoleContent = document.querySelector('.profile__subtitle').textContent;
 const defaultName = document.querySelector('.profile__name');
 const defaultRole = document.querySelector('.profile__subtitle');
 
-function closeModal() { 
-    modal.classList.remove('modal_display_visible');
-    modal.classList.add('modal_display_none');
+// function toggleModal() { 
+//     modal.classList.remove('modal_display_visible');
+//     modal.classList.add('modal_display_none');
+// }
+
+function toggleModal() {
+    modal.classList.toggle('modal_display_visible')
 }
 
-function openModal() {
-    modal.classList.remove('modal_display_none');
-    modal.classList.add('modal_display_visible');
-}
+editButton.addEventListener("click", toggleModal);
 
-editButton.addEventListener("click", openModal);
-
-closeEditButton.addEventListener('click', closeModal);
-
-profileName.setAttribute("value", profileNameContent);
-profileRole.setAttribute("value", profileRoleContent);
+closeEditButton.addEventListener('click', toggleModal);
 
 profileSaveButton.addEventListener("click", function() {
     
     defaultName.textContent = profileName.value;
     defaultRole.textContent = profileRole.value;
 
-    closeModal();
+    toggleModal();
 })
